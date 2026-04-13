1f8d274173948819d6aac74800624011200dab06
jurufino <juliaruffiino@gmail.com>
2026-04-13 20:58:51 -0300

    Cria novas listas automaticamente com redirecionamento

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,27 +1,49 @@
+
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 
-items = []
+
+lists = {}
+
+next_id = 1
 
 def home_page(request):
+    global next_id
+
+    item = request.GET.get('item_text')
+
+    if item:
+        list_id = next_id
+        next_id += 1
+
+        lists[list_id] = [item]
+
+        return HttpResponseRedirect(f'/lists/{list_id}/')
+
     return HttpResponse('''
         <html>
             <title>To-Do lists</title>
             <body>
                 <h1>Welcome to To-Do App</h1>
-                <a href="/lists/">Go to your list</a>
+
+                <form method="GET">
+                    <input name="item_text" placeholder="Enter a to-do item" />
+                </form>
             </body>
         </html>
     ''')
 
-
 def view_list(request, list_id):
     item = request.GET.get('item_text')
 
+    if list_id not in lists:
+        lists[list_id] = []
+
     if item:
-        items.append(item)
+        lists[list_id].append(item)
 
     list_html = ''
-    for i, item in enumerate(items, start=1):
+    for i, item in enumerate(lists[list_id], start=1):
         list_html += f'<tr><td>{i}: {item}</td></tr>'
 
     return HttpResponse(f'''
