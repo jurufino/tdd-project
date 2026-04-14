d68ae7e49ac09d67c37e68afb85ea5afd871cae2
jurufino <juliaruffiino@email.com>
2026-04-14 00:52:17 -0300

    Impede criação de itens vazios na lista

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+
 class List(models.Model):
     pass
 
+
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
-    
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if self.text == '':
+            raise Exception("Não pode salvar item vazio")
+        super().save(*args, **kwargs)
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from lists.models import Item, List
 
-# Create your tests here.
+
+class ItemModelTest(TestCase):
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+
+        item = Item(list=list_, text='')
+        
+        with self.assertRaises(Exception):
+            item.save()
