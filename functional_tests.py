84ebedb5d166e57689dd65872afe76a276c1173f
jurufino <juliaruffiino@email.com>
2026-04-14 01:03:03 -0300

    Adiciona teste funcional para validação de entrada vazia

--- functional_tests.py
+++ functional_tests.py
@@ -1,44 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
+browser = webdriver.Chrome()
 
-def test_can_start_a_list_and_retrieve_it_later():
-    browser = webdriver.Chrome()
-
+try:
+    # Abrir home
     browser.get('http://127.0.0.1:8000')
 
-    inputbox = browser.find_element(By.TAG_NAME, 'input')
+    # Encontrar input e enviar vazio
+    inputbox = browser.find_element(By.NAME, 'item_text')
+    inputbox.send_keys('')
+    inputbox.submit()
 
-    inputbox.send_keys('Comprar pão')
-    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
 
+    # Verificar se mensagem de erro aparece
+    body_text = browser.find_element(By.TAG_NAME, 'body').text
+
+    assert "Você não pode enviar um item vazio" in body_text
+
+finally:
     time.sleep(5)
     browser.quit()
-
-    assert '/lists/' in browser.current_url
-
-    table = browser.find_element(By.TAG_NAME, 'body')
-    assert 'Comprar pão' in table.text
-
-    browser.get('http://127.0.0.1:8000')
-
-    inputbox = browser.find_element(By.TAG_NAME, 'input')
-    inputbox.send_keys('Estudar Django')
-    inputbox.send_keys(Keys.ENTER)
-
-    time.sleep(5)
-    browser.quit()
-
-    new_url = browser.current_url
-    assert new_url != 'http://127.0.0.1:8000/lists/1/'
-
-    body = browser.find_element(By.TAG_NAME, 'body')
-    assert 'Estudar Django' in body.text
-
-    browser.quit()
-
-
-if __name__ == "__main__":
-    test_can_start_a_list_and_retrieve_it_later()
