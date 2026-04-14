70703efa6fa5a54d230755fad4d01d9658d64b53
jurufino <juliaruffiino@email.com>
2026-04-14 00:40:04 -0300

    feat: adiciona criação de novas listas e separa views

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,22 +6,26 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(
-        text=request.POST.get('item_text'),
-        list=list_
-    )
-    return redirect(f'/lists/{list_.id}/')
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
 
     if request.method == 'POST':
         item_text = request.POST.get('item_text')
-        if item_text:
-            Item.objects.create(text=item_text, list=list_)
+
+        if not item_text:
+            items = Item.objects.filter(list=list_)
+            return render(request, 'list.html', {
+                'list': list_,
+                'items': items,
+                'error': "You can't have an empty list item"
+            })
+
+        Item.objects.create(
+            text=item_text,
+            list=list_
+        )
+
+        return redirect(f'/lists/{list_id}/')
 
     items = Item.objects.filter(list=list_)
 
@@ -29,3 +33,17 @@ def view_list(request, list_id):
         'list': list_,
         'items': items
     })
+
+
+def new_list(request):
+    list_ = List.objects.create()
+
+    item_text = request.POST.get('item_text')
+
+    if item_text:
+        Item.objects.create(
+            text=item_text,
+            list=list_
+        )
+
+    return redirect(f'/lists/{list_.id}/')
